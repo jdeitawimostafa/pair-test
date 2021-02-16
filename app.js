86d57efb3bf5c1a13b79366cf0d39ea2d266b2c4
mostafakjdeitawi <mostafagedo0785@gmail.com>
2021-02-16 15:18:44 -0800

    test

---
+++ app.js
@@ -0,0 +1,11 @@
+'use srtict'
+
+let firstname = prompt('what is your first name ? ');
+let lastname = prompt('what is your last name ? ');
+
+function printFullname (firstname , lastname){
+    console.log(firstname , lastname)
+    return ('first name' , 'last name');
+}
+
+printFullname(firstname , lastname);
